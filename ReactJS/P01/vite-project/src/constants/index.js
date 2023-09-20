44db24d0f5bf0fdeaac87d3ae29f078ac6ce1b42
Vikas Parmar <122597785+vikas-parmar@users.noreply.github.com>
2023-09-20 13:25:19 +0000

    Made Data for the Nutrition table

---
+++ ReactJS/P01/vite-project/src/constants/index.js
@@ -0,0 +1,132 @@
+export const nutritions = [
+    {
+        id: 1,
+        title: "Frozen Yoghurt",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 2,
+        title: "Ice Cream Sandwich",
+        categories: {
+            calories: 237,
+            fat: 9,
+            carbs: 37,
+            protein: 4.3,
+        },
+    },
+    {
+        id: 3,
+        title: "Eclair",
+        categories: {
+            calories: 262,
+            fat: 16,
+            carbs: 24,
+            protein: 6,
+        },
+    },
+    {
+        id: 4,
+        title: "Cupcake",
+        categories: {
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+        },
+    },
+    {
+        id: 5,
+        title: "Marshmallow",
+        categories: {
+            calories: 318,
+            fat: 0,
+            carbs: 81,
+            protein: 2,
+        },
+    },
+    {
+        id: 6,
+        title: "Grilled Chicken Salad",
+        categories: {
+            calories: 300,
+            fat: 10,
+            carbs: 15,
+            protein: 25,
+        },
+    },
+    {
+        id: 7,
+        title: "Quinoa and Black Bean Bowl",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 8,
+        title: "Greek Yogurt Parfait",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 9,
+        title: "Vegetable Stir-Fry with Tofu",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 10,
+        title: "Lentil Soup",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 11,
+        title: "Baked Salmon",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 12,
+        title: "Frozen yoghurt",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+    {
+        id: 13,
+        title: "Spinach and Feta Stuffed Chicken Breast",
+        categories: {
+            calories: 159,
+            fat: 6,
+            carbs: 24,
+            protein: 4,
+        },
+    },
+]
